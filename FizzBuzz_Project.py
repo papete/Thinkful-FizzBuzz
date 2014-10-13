0d4008b5738d7b59ffc5705b84e233fedfce00b2
Patrick Pete <ppete@ucanmediatech.com>
2014-10-13 16:17:29 -0400

    Prob working on correctly using for loop in FizzBuzz prj
    This commit is related to the work my Thinkful mentor and I worked on
    understanding how to use control flow, functions and committing to Git.

--- FizzBuzz_Project.py
+++ FizzBuzz_Project.py
@@ -1,7 +1,14 @@
-import sys
 
-numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+def fizzbuzz(numbers):
+	for number in numbers:
+		if number % 3 == 0 and number % 5 == 0:
+		   print "fizzbuzz and", number
+		elif number % 3 == 0:
+			 print "fizz"
+		elif number % 5 == 0:
+			 print "buzz"
 
-for number in numbers:
-	if number 
-	print "Fizz buzz counting up %s" % number
+if __name__ == "__main__":
+   print "Main is being called"
+   numbers = range(1,100)
+   fizzbuzz(numbers)
